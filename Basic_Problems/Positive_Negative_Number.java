28f608b14a9061344ffb620d8fdf35bd9290b776
Banty6818 <riteshbodkhe6818@gmail.com>
2024-08-21 02:09:06 +0530

    Basic_Problems

---
+++ Basic_Problems/Positive_Negative_Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Positive_Negative_Number {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+
+       if(n>0){
+            System.out.println("Number is positive.");
+       }
+       else{
+            System.out.println("Number is negative.");
+       }
+    }
+}
