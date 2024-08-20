7649da20ae38ff6b87e5e2e1a47fd6e2ea2b76c7
Banty6818 <riteshbodkhe6818@gmail.com>
2024-08-21 01:11:45 +0530

    Basic_Problems

--- Basic_Problems/Even_Odd.java
+++ Basic_Problems/Even_Odd.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class Even_Odd {
     public static void main(String args[]){
-        int n=2;
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
         if(n%2==0){
             System.out.println(n+" is even number.");
         }
