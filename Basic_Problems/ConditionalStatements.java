bf9164fcd12d0ee68bccbc5c2e916569fffb2dc0
Banty6818 <riteshbodkhe6818@gmail.com>
2024-08-21 01:00:05 +0530

    Basic_Problems

---
+++ Basic_Problems/ConditionalStatements.java
@@ -0,0 +1,13 @@
+public class ConditionalStatements {
+    public static void main(String args[]){
+
+        // if-else conditions
+        int a = 21;
+        if(a>18){
+            System.out.println("You can vote.");
+        }
+        else{
+            System.out.println("You can't vote.");
+        }
+    }
+}
