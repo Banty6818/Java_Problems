2ffe81dda37bf17b3be37e8e98e464c3eab77b97
Banty6818 <riteshbodkhe6818@gmail.com>
2024-08-24 15:13:49 +0530

    Basic_Problems

---
+++ Basic_Problems/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+        // while loop
+        int x = 0;
+        while(x>10){
+            System.out.print(x+" ");
+            x++;
+        }
+        System.out.println();
+    }
+}
