d7ff345609543ea8775427640d65dd75605b87b6
Banty6818 <riteshbodkhe6818@gmail.com>
2024-09-23 16:19:46 +0530

    Pattern

---
+++ Pattern/char_pattern.java
@@ -0,0 +1,21 @@
+package Java_Problems.Pattern;
+import java.util.*;
+
+public class char_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        char ch = 'A';
+
+        System.out.print("Enter Number:");
+        n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
