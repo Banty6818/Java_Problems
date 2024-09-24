2876d740d1cf922f353beff4db11ea373fdda459
Banty6818 <riteshbodkhe6818@gmail.com>
2024-09-24 14:29:20 +0530

    Pattern

---
+++ Function/Calculate_Sum.java
@@ -0,0 +1,12 @@
+package Java_Problems.Function;
+
+public class Calculate_Sum {
+    public static void Sum(int a, int b) {
+        int sum = a + b;
+        System.out.println(a + " + " + b + " : " + sum);
+    }
+    public static void main(String[] args) {
+        Sum(4, 5);
+        Sum(4, 4);
+    }
+}
