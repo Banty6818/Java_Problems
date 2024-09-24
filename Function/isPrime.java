153ecae4f5ff649f93e60944515313675ff24e42
Banty6818 <riteshbodkhe6818@gmail.com>
2024-09-25 00:50:22 +0530

    Function

--- Function/isPrime.java
+++ Function/isPrime.java
@@ -11,8 +11,24 @@ public class isPrime {
         }
         return isPrime;
     }
+
+    // Optimized Solution use range from 2 to square root of n
+    public static boolean isPrime_number(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
         boolean isprime = isPrime(2);
         System.out.println("Number is "+isprime);
+        boolean isprime1 = isPrime_number(16);
+        System.out.println("Number is "+isprime1);
     }
 }
