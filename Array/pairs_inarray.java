789de451a94d18968aa2a867ac542026e04b5462
Banty6818 <riteshbodkhe6818@gmail.com>
2024-09-27 12:36:00 +0530

    Array

---
+++ Array/pairs_inarray.java
@@ -0,0 +1,16 @@
+package Java_Problems.Array;
+
+public class pairs_inarray {
+    public static void pairs(int numbers[]) {
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                System.out.print("("+numbers[i]+", "+numbers[j]+") ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        pairs(numbers);
+    }
+}
