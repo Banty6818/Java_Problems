8e7483f4cfd0cde2d24a620d5d7b8e91ea76ecb2
Banty6818 <riteshbodkhe6818@gmail.com>
2024-09-29 00:24:12 +0530

    Array

--- Array/kadanes_algorithm.java
+++ Array/kadanes_algorithm.java
@@ -1,3 +1,22 @@
 public class kadanes_algorithm {
-    
+    public static int kadanes_algo(int numbers[]) {
+        int curr_sum = 0;
+        int max_sum = Integer.MIN_VALUE;
+
+        for(int i=0; i<numbers.length; i++){
+            curr_sum = curr_sum + numbers[i];
+            if(curr_sum < 0){
+                curr_sum = 0;
+            }
+            if(curr_sum>max_sum){
+                max_sum = curr_sum;
+            }
+        }
+        return max_sum;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {5, 9, -2, 3, 7, -6, -5};
+        int max = kadanes_algo(numbers);
+        System.out.println("Max Sum = "+max);
+    }
 }
