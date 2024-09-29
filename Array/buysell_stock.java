34d35d7a1afd887fffe3ae0f634300eb50ad34e5
Banty6818 <riteshbodkhe6818@gmail.com>
2024-09-29 17:33:21 +0530

    Array

---
+++ Array/buysell_stock.java
@@ -0,0 +1,22 @@
+public class buysell_stock {
+    public static int buyAndsellstock(int price[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<price.length; i++){
+            if(buyPrice < price[i]){
+                int profit = price[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else{
+                buyPrice = price[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]){
+        int price[] = {7, 1, 5, 3, 6, 4};
+        int maxProfit = buyAndsellstock(price);
+        System.out.println("Maximum Profit = "+maxProfit);
+    }
+}
