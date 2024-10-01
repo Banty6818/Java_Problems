a41f448ba6c8d2355e0cab258d0772b3863664ae
Banty6818 <riteshbodkhe6818@gmail.com>
2024-10-02 00:05:02 +0530

    Array

--- Array/triplet.java
+++ Array/triplet.java
@@ -1,17 +1,28 @@
-public class triplet{
+import java.util.Arrays;
+
+public class triplet {
     public static void triplet(int numbers[]) {
-        int[] result = new int[numbers.length];
-        for(int i=0; i<numbers.length; i++){
-            for(int j=i+1; j<numbers.length; j++){
-                for(int k=j+1; k<numbers.length; k++){
-                    int sum = numbers[i] + numbers[j] + numbers[k];
-                    if(sum==0){
-                        System.out.println("["+numbers[i]+", "+numbers[j]+", "+numbers[k]+"]");
-                    }                
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 2; i++) {
+            if (i > 0 && numbers[i] == numbers[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < numbers.length - 1; j++) {
+                if (j > i + 1 && numbers[j] == numbers[j - 1]) {
+                    continue;
+                }
+                for (int k = j + 1; k < numbers.length; k++) {
+                    if (k > j + 1 && numbers[k] == numbers[k - 1]) {
+                        continue;
+                    }
+                    if (numbers[i] + numbers[j] + numbers[k] == 0) {
+                        System.out.println("[" + numbers[i] + ", " + numbers[j] + ", " + numbers[k] + "]");
+                    }
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         int numbers[] = {-1, 0, 1, 2, -1, 4};
         triplet(numbers);
